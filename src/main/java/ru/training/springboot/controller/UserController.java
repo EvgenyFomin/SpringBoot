935e52b05113073a83b0c77e00998776c9d533c9
EvgenyFomin <sevenup96@mail.ru>
2018-09-02 22:06:49 +0500

    TODO

---
+++ src/main/java/ru/training/springboot/controller/UserController.java
@@ -0,0 +1,23 @@
+package ru.training.springboot.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.training.springboot.repository.UserRepository;
+
+@Controller
+@RequestMapping(name = "/user")
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public String userList(Model model) {
+        model.addAttribute("users", userRepository.findAll());
+
+        return "userList";
+    }
+
+}
