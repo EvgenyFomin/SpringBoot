6b640012117a51502433e7da19a6cf0ac7b8b658
EvgenyFomin <sevenup96@mail.ru>
2018-09-03 16:41:35 +0500

    addUploadFile

--- src/main/java/ru/training/springboot/controller/UserController.java
+++ src/main/java/ru/training/springboot/controller/UserController.java
@@ -1,17 +1,22 @@
 package ru.training.springboot.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.training.springboot.domain.Role;
 import ru.training.springboot.domain.User;
 import ru.training.springboot.repository.UserRepository;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @Controller
 @RequestMapping("/user")
+@PreAuthorize("hasAuthority('ADMIN')")
 public class UserController {
     @Autowired
     private UserRepository userRepository;
@@ -29,5 +34,26 @@ public class UserController {
         model.addAttribute("roles", Role.values());
 
         return "userEdit";
+    }
+
+    @PostMapping
+    public String userSave(@RequestParam String username,
+                           @RequestParam Map<String, String> form,
+                           @RequestParam("userId") User user) {
+
+        user.setUsername(username);
+
+        Set<String> roles = Arrays.stream(Role.values()).map(Role::name).collect(Collectors.toSet());
+
+        user.getRoles().clear();
+
+        for (String currentKey : form.keySet()) {
+            if (roles.contains(currentKey)) {
+                user.getRoles().add(Role.valueOf(currentKey));
+            }
+        }
+
+        userRepository.save(user);
+        return "redirect:/user";
     }
 }
--- src/main/resources/templates/userEdit.ftl
+++ src/main/resources/templates/userEdit.ftl
@@ -6,11 +6,11 @@
     <input type="text" name="username" value="${user.username}">
     <#list roles as role>
         <div>
-            <label><input type="checkbox" name="role" ${user.roles?seq_contains(role)?string("checked", "")}>${role}
+            <label><input type="checkbox" name="${role}" }>${role}
             </label>
         </div>
     </#list>
-    <input type="text" name="userId" value="${user.id}">
+    <input type="hidden" name="userId" value="${user.id}">
     <input type="hidden" name="_csrf" value="${_csrf.token}">
     <button type="submit">Save</button>
 </form>
